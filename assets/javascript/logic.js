254d29683abaef8fa4356875cdacc38954e7fde4
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-06-27 21:19:10 -0400

    these are the changes with the location address field converting to lat and long coordinates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,47 +1,76 @@
+$(document).ready(function() {
+
+    // Load google map
+    var map = new google.maps.Map(document.getElementById("map-section"), {
+        center: new google.maps.LatLng(38.883340, -77.117982),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false
+    });
 
 
-//INIT EVENT HANDLER SANDBOX
-      function initMap() {
-      var mapProp = {
-      center: new google.maps.LatLng(38.883340, -77.117982),
-      zoom: 15,
-      };
-      var map = new google.maps.Map(document.getElementById("map-section"),mapProp);
-}
+    $("#submit-bathroom").on("click", function(event) {
+        // Prevent the default action of the element from happening
+        event.preventDefault();
 
-      
- $("#submit-bathroom").on("click", function(event) { 
-        //prevent the default action of the element from happening
-            event.preventDefault();
-       
-        //inside the form input, use jq to grab the location input without spaces  
+        // Inside the form input, use jq to grab the location input without spaces  
         var name = $("#name-input").val().trim();
-        var review= $("#review-input").val().trim();
-        var currentLocation = $("#current-location-input").val().trim(); 
-        var image = $("#image-input").val().trim();
-
-
-
-        // Clear the input form
         $("#name-input").val("");
+
+        var review = $("#review-input").val().trim();
         $("#review-input").val("");
+
+        var currentLocation = $("#current-location-input").val().trim();
         $("#current-location-input").val("");
+
+        var image = $("#image-input").val().trim();
         $("#image-input").val("");
 
-        //the location from the text box has to be queued up in the array 
-        var a = $("<button>"); 
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+                address: currentLocation,
+                region: 'no'
+            },
 
-          //for the button stored in variable a, add a class of location 
-          a.addClass("location"); 
+            function(results, status) {
+                var name = $("#name-input").val().trim();
+                var review = $("#review-input").val().trim();
+                var currentLocation = $("#current-location-input").val().trim();
+                var image = $("#image-input").val().trim();
 
-          //for the button stored in variable a, add the data attribute 
-          a.attr("data-location", location); 
+                if (status.toLowerCase() == 'ok') {
+                    var coordinates = new google.maps.LatLng(
+                        results[0]['geometry']['location'].lat(),
+                        results[0]['geometry']['location'].lng()
+                    );
 
-          //for the button stored in variable a, write the text of the location 
-          a.text(location); 
+                    map.setCenter(coordinates)
+                    map.setZoom(13);
 
-          //use jq to grab the buttons view div and add the button to it
-          //this is how we control the view, whether it is removed or added, etc.
-          $("#buttons-view").append(a); 
+                    marker = new google.maps.Marker({
+                        position: coordinates,
+                        map: map,
+                        // title: $('#current-location').val()
+                    });
+                }
+            });
+    });
+});
 
-    }); 
+// //the location from the text box has to be queued up in the array 
+// var a = $("<button>"); 
+
+//   //for the button stored in variable a, add a class of location 
+//   a.addClass("location"); 
+
+//   //for the button stored in variable a, add the data attribute 
+//   a.attr("data-location", location); 
+
+//   //for the button stored in variable a, write the text of the location 
+//   a.text(location); 
+
+//   //use jq to grab the buttons view div and add the button to it
+//   //this is how we control the view, whether it is removed or added, etc.
+//   $("#buttons-view").append(a);
