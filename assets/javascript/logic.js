abcd29bdeaa76638118921c58aa488bf5ec2012f
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-06-28 23:41:07 -0400

    added working firebase skeleton to build upon

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,3 @@
-
-
 // Initilize Google Maps API and displaying to the page
 function initMap() {
     var defaultPosition = new google.maps.LatLng(38.883340, -77.117982);
@@ -35,6 +33,42 @@ function initMap() {
         $("#image-input").val("");
 
 
+        // Setting up our firebase configuration
+        var config = {
+            apiKey: "AIzaSyAGDsGNskbjVbwEFV8Ww9nINcsKZ0YgfEY",
+            authDomain: "ohhh-crapp.firebaseapp.com",
+            databaseURL: "https://ohhh-crapp.firebaseio.com",
+            projectId: "ohhh-crapp",
+            storageBucket: "ohhh-crapp.appspot.com",
+            messagingSenderId: "286848805874"
+        };
+
+        firebase.initializeApp(config);
+
+        var database = firebase.database();
+
+
+        // Setting the object to store our information
+        var newPost = {
+            name: name,
+            address: address,
+            review: review,
+            image: image,
+        };
+
+
+        // Pushing our input values to a new object for each submission
+        database.ref().push(newPost);
+
+        database.ref().on("child_added", function(snapshot) {
+            name: snapshot.val().name;
+            address: snapshot.val().address;
+            review: snapshot.val().review;
+            image: snapshot.val().image
+            // Write this information to the marker and/or page somewhere
+        });
+
+
         // Setting up the geocoder so when we type in the physical address it will convert to lattitude and longitude coordinates for us
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({
@@ -44,8 +78,8 @@ function initMap() {
 
             function(results, status) {
                 var name = $("#name-input").val().trim();
-                var review = $("#review-input").val().trim();
                 var address = $("#address-input").val().trim();
+                var review = $("#review-input").val().trim();
                 var image = $("#image-input").val().trim();
 
 
@@ -81,7 +115,7 @@ function initMap() {
 
 
                     // Setting the variable that will store the content the user types in so we can store it in an information window, need to fix this
-                    var contentString = 'Name: ' + 'name value' + '<br>' + 'Address: ' + 'address value' + '<br>' +'<br>' + 'image' + '<br>' +'<br>' + 'review value';
+                    var contentString = 'Name: ' + 'name value' + '<br>' + 'Address: ' + 'address value' + '<br>' + '<br>' + 'image' + '<br>' + '<br>' + 'review value';
 
 
                     // Setting up the information window
