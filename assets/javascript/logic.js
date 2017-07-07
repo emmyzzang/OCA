86590e334bf071c8ccb6a8b10e100b7b7daf96f8
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-07-07 19:39:42 -0400

    making search function use index of to pull even if it isnt the full string

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,18 +5,17 @@ function initMap() {
     var autocompleteTop = new google.maps.places.Autocomplete(
         (document.getElementById('location-input')), { types: ['geocode'] });
 
-    var defaultPosition = new google.maps.LatLng(38.883340, -77.117982);
+    var defaultPosition = new google.maps.LatLng(37.883340, -97.117982);
 
     var map = new google.maps.Map(document.getElementById("map-section"), {
         center: defaultPosition,
-        zoom: 7,
+        zoom: 5,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         panControl: false,
         streetViewControl: false,
         mapTypeControl: false,
     });
 
-    // Set up firebase configuration
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyDuxG2pJypuXV0shA-4H91vskgRNS-FY1k",
@@ -35,35 +34,25 @@ function initMap() {
 
     // Event listener for button for user to query existing bathroom database 
     // for bathroom location that the user is trying to find 
-    $("#add-location").click(function(event) {
+    $("#search-location").click(function(event) {
         event.preventDefault();
 
         // Grab the user input and store into declarative variable
-        var newLocation = $("#location-input").val().trim();
-
+        var newLocation = $("#location-input").val().toUpperCase().trim();
+        console.log(newLocation);
         newObject = {
             location: newLocation
         };
+
         // Query db with event handler and pass a snapshot event thru fcn 
         // .val() grabs the childSnapshot behind it and passes it thru the fcn forEach 
         var leadsRef = database.ref('myData');
         leadsRef.on('value', function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 var childData = childSnapshot.val();
-                if (newLocation == childData.address || newLocation == childData.name) {
-                    //Update map function to update map! 
+                if (childData.address.indexOf(newLocation) > -1 || childData.name.indexOf(newLocation) > -1) {
                     updateMap(childData);
                 } 
-
-                // else {
-                //     function error() {
-                //         $('#error-message').html('Entry not found!')
-                //     }
-                //     error();
-                //     $('#error-message').fadeOut(4000);
-                //     $("#location-input").val("");
-
-                // }
             });
         });
 
@@ -71,20 +60,24 @@ function initMap() {
     });
 
 
-    //**USER SUBMIT DATA TO DATABASE**// 
+    // Click event to submit a new post to the database
     $("#submit-bathroom").on("click", function(event) {
         event.preventDefault();
 
         // Set the variables to correspond to the input fields
-        var name = $("#name-input").val().trim();
-        var review = $("#review-input").val().trim();
-        var address = $("#address-input").val().trim();
+        var name = $("#name-input").val().toUpperCase().trim();
+        var review = $("#review-input").val().toUpperCase().trim();
+        var address = $("#address-input").val().toUpperCase().trim();
+
         // Image input needs to be grabbed from a collection of files  
         var fileCollection = $("#image-input");
+
         // The option to select an entire file collection if we really wanted to
         var file = fileCollection[0].files[0];
+
         // Need to declare what object the child is comprised of, and store in declarative var 
         var fileName = file.name;
+
         // Define the upload task to ref the path of the file in db
         var uploadTask = firebase
             .storage()
@@ -110,7 +103,7 @@ function initMap() {
                 var imageURL = uploadTask.snapshot.downloadURL;
 
                 // Declare the object to store our information
-                //This is staging the post as a JSON object to be pushed to db
+                // This is staging the post as a JSON object to be pushed to db
                 var newPost = {
                     name: name,
                     address: address,
@@ -122,7 +115,6 @@ function initMap() {
                 database.ref().child('myData').push(newPost);
 
                 // Add each submission to our database
-                // .val() grabs the snapshot of each form element and updates it 
                 database.ref().on("child_added", function(snapshot) {
                     name: snapshot.val().name;
                     address: snapshot.val().address;
@@ -141,8 +133,8 @@ function initMap() {
             });
     });
 
-
     function updateMap(newPost) {
+
         // Geocoder will convert the address string to latitude & longitude
         $("#location-input").val("");
         var geocoder = new google.maps.Geocoder();
@@ -173,7 +165,7 @@ function initMap() {
                     );
 
                     map.setCenter(defaultPosition)
-                    map.setZoom(7);
+                    map.setZoom(5);
 
                     marker = new google.maps.Marker({
                         position: coordinates,
@@ -188,9 +180,6 @@ function initMap() {
                     marker.addListener('click', function() {
                         $('#myModal').modal('show');
                         $('#marker-info').html(contentString)
-                        $('location-header').html(newPost.address.toUpperCase())
-                            // infowindow.setContent()
-                            // infowindow.open(map, this);
                     });
                 }
             });
